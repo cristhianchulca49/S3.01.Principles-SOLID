9dc32e8f66151252fb36e86898590d82ea177ba6
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-12 13:16:03 +0100

    feat: add List of instruments and call to playInstrument() method. Remove unnecessary code

--- Tasca-S3-0/O/src/Main.java
+++ Tasca-S3-0/O/src/Main.java
@@ -1,20 +1,15 @@
-public class Main {
-    public void play(String instrument) {
-        if ("guitar".equals(instrument)) {
-            System.out.println("🎸 Strumming the guitar");
-        } else if ("drums".equals(instrument)) {
-            System.out.println("🥁 Beating the drums");
-        } else if ("piano".equals(instrument)) {
-            System.out.println("🎹 Playing the piano");
-        } else {
-            System.out.println("🔇 Unknown instrument");
-        }
-    }
+import Models.*;
 
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
-        Main player = new Main();
-        player.play("guitar");
-        player.play("drums");
-        player.play("piano");
+        List<Instruments> instruments = List.of(
+                new Drums(),
+                new Guitar(),
+                new Piano()
+        );
+
+        instruments.forEach(Play::playInstrument);
     }
 }
