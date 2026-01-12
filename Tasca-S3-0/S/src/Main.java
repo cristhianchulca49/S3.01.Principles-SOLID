b8b46b7a3f1d2915a7ee5140a0d2342cb5a69c6c
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-12 11:47:00 +0100

    feat: create instances and call methods

--- Tasca-S3-0/S/src/Main.java
+++ Tasca-S3-0/S/src/Main.java
@@ -1,2 +1,19 @@
+import Model.User;
+import Services.EmailConfirmation;
+import Services.EmailValidation;
+import Services.Exceptions.FailConfirmationException;
+
 public class Main {
+    public static void main(String[] args) throws FailConfirmationException {
+
+        EmailValidation emailValidation = new EmailValidation();
+        EmailConfirmation emailConfirmation = new EmailConfirmation();
+
+        User user = new User("Josep", "joser@ITAcademy.es", "ABC123");
+
+        emailValidation.validation(user);
+
+        String message = "📧 Sending confirmation email to: ";
+        emailConfirmation.sendEmailConfirmation(message, user);
+    }
 }
