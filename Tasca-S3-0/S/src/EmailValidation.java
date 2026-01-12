5143c5ad5bafd4481c7f7b73b0fed3a7d1d4216e
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-12 11:15:46 +0100

    refactor: rename method to validateEmail() asking an User for parameter

--- Tasca-S3-0/S/src/EmailValidation.java
+++ Tasca-S3-0/S/src/EmailValidation.java
@@ -1,3 +1,12 @@
 public class EmailValidation {
 
+    public void validateEmail(User user) {
+        if (user.email() == null || !user.email().contains("@") || !user.email().contains(".")) {
+            throw new IllegalArgumentException("Invalid email address.");
+        }
+
+        if (user.password() == null || user.password().length() < 8 || !user.password().matches(".*[A-Z].*")) {
+            throw new IllegalArgumentException("Password must be at least 8 characters long and contain an uppercase letter.");
+        }
+    }
 }
